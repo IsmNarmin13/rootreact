d9b253b871397f317bcee0d244e0083af7d37c28
ismnarmin13 <narminvi@code.edu.az>
2023-12-29 14:28:23 +0400

    add

--- src/index.js
+++ src/index.js
@@ -2,16 +2,40 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ContactsPage from "./components/ContactsPage";
+import NoPage from "./components/NoPage";
+import About from './components/About';
+import Index from './components/Index';
+
+import { BrowserRouter, Routes, Route, Link, NavLink } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <BrowserRouter>
     <App />
+
+    <nav>
+        <ul>
+          <li>
+            <NavLink to="/">Index</NavLink>
+          </li>
+          <li>
+            <NavLink to="/contactspage">ContactsPage</NavLink>
+          </li>
+          <li>
+            <NavLink to="/about">About</NavLink>
+          </li>
+        </ul>
+      </nav>
+
+
+      <Routes>
+        <Route path="/" element={<Index />} />
+        <Route path="contactspage" element={<ContactsPage />} />
+        <Route path="about" element={<About />} />
+        <Route path="*" element={<NoPage />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
